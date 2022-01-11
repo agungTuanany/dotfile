628daa984b5f1b2e9830af681093962a607b1ecf
agungTuanany <agung.tuanany@gmail.com>
2022-01-11 22:30:44 +0700

    emacs [config]: complete emacs From Scratch #8 Build your Own IDE /w lsp-mode -3-d
    - evil-nerd-commenter
    
      Comment/uncomment lines efficiently. Like Nerd Commenter in Vim
    
    - rainbow-delimeters
      Highlight nested parentheses and brackets according to their nesting depth.
      This makes it a lot easier to visually match parentheses in Emacs Lisp code without having to count them yourself.
    
    ref: - http://github.com/redguardtoo/evil-nerd-commenter
         - https://github.com/Fanael/rainbow-delimiters

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -486,16 +486,9 @@
 ;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
 (use-package forge
   :after magit)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(lsp-ivy lsp-treemacs xref-js2 which-key use-package typescript-mode lsp-ui js2-refactor ivy-rich hydra helpful general forge evil-surround evil-collection doom-themes doom-modeline counsel-projectile company-box command-log-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(use-package evil-nerd-commenter
+  :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
