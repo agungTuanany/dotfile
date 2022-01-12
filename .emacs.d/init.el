5d8ff3ef80d5e84d568d73012dda0de3304c9689
agungTuanany <agung.tuanany@gmail.com>
2022-01-12 11:18:54 +0700

    emacs [config]: complete emacs From Scratch #9 Terminal Modes
    - term-mode
    
      If you using 'term-mode' and you using some program that where the
      keys aren't working correctly you may need to switch to
      'term-char-mode' (C-c C-k) and you can go back to 'term-line-mode'
      with (C-c C-j).
    
      You can see in the right corner of your window 'Term: char run' or
      'Term: line run'.
    
      If you have 'evil-collection' installed, 'term-mode' will enter char
      mode when you use Evil's Insert mode
    
    - eterm-256color
    
      The 'eterm-256color' package enhances the output of 'term-mode' to
      enable handling of a wider range of color codes so that many popular
      terminal applications look as you would expect them to.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -492,3 +492,28 @@
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package term
+  :commands term
+  :config
+  (setq explicit-shell-file-name "bash") ;; Change this to zsh, etc
+  ;;(setq explicit-zsh-args '())         ;; Use 'explicit-<shell>-args for shell-specific args
+
+  ;; Match the default Bash shell prompt.  Update this if you have a custom prompt
+  (setq term-prompt-regexp "^[^#$%>\n]*[#$%>] *"))
+
+(use-package eterm-256color
+  :hook (term-mode . eterm-256color-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(eterm-256color rainbow-delimiters xref-js2 which-key use-package typescript-mode lsp-ui lsp-treemacs lsp-ivy js2-refactor ivy-rich helpful general forge evil-surround evil-nerd-commenter evil-collection doom-themes doom-modeline counsel-projectile company-box command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
