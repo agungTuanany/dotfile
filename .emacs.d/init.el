fd296c74d40c5489daaf438480b494a302d5a54e
agungTuanany <agung.tuanany@gmail.com>
2022-01-15 11:30:50 +0700

    emacs [config]: complete emacs From Scratch #10 Effortless File Management with 'dired' -1
    base dired config
    
    - mapping dired-mode into evil-mode key in 'normal mode'
    
      ```
        (evil-collection-define-key 'normal 'dired-mode-map
          "h" 'dired-single-up-directory
          "l" 'dired-single-buffer))
      ```
    
    - configuring dired to organize directory first to show up
    
    ```
      :custom ((dired-listing-switches "-agho --group-directories-first"))
    ```
    
    ref:
    - System Crafter:
      - https://www.youtube.com/watch?v=PMWwM8QJAtU
      - https://github.com/daviwil/emacs-from-scratch/blob/master/show-notes/Emacs-10.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -542,3 +542,13 @@
     (setq eshell-visual-commands '("htop" "zsh" "vim")))
 
   (eshell-git-prompt-use-theme 'powerline))
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-agho --group-directories-first"))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-up-directory
+    "l" 'dired-find-file))
