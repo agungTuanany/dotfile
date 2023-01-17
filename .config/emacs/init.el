ec8e19a69e76106e5c57dc4c5cfeccd4c04a1aa7
agungTuanany <agung.tuanany@gmail.com>
2023-01-17 17:02:16 +0700

    refactor [emacs]: emacs configuration from scratch
    credit:
    [system crafter]https://www.youtube.com/watch?v=51eSeqcaikM&list=PLEoMzSkcN8oNmd98m_6FoaJseUsa6QGm2&index=3&t=14s

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,9 @@
+;;; Package -- init.el
+;;
+;;; Commentary:
+;; use systemlinks from the repo:
+;; ln -s ~/Repo/agung_dotfile/.config/emacs ~/config/
+
 ;;=======================================
 ;;; Presetup Begin
 (setq inhibit-startup-message t
@@ -5,11 +11,13 @@
 
 (scroll-bar-mode -1)       ; Disable visible scrollbar
 (tool-bar-mode -1)         ; Disable toolbar
-(tooltip-mode -1)
-(menu-bar-mode -1)
-(set-fringe-mode 10)
+(tooltip-mode -1)          ; Disable tootips
+(menu-bar-mode -1)         ; Disbale menu on top
+(set-fringe-mode 10)       ; Give some breathing rome
 (electric-pair-mode 1)     ; auto-closing brackets
 (auto-revert-mode 1)       ; refresh the file if has changedee
+
+(add-to-list 'default-frame-alist '(fullscreen, maximzed))
 
 ;; line-number config
 (column-number-mode)
@@ -31,23 +39,73 @@
 
 (setq-default fill-colum 80)
 
+;; theme config
 (load-theme 'tango-dark t)
 
-;;========================================
+;;(set-face-attribute 'default nil :font "Liberation Mono" :height 90)
+(set-face-attribute 'default nil :font "Source Code Pro" :height 90)
+
+;; keybinding config
 ;; Make <ESC> quit prompt either to <C-g>
 (global-set-key (kbd "<escapge>") 'keyboard-escape-quit)
 
-(setq custom-file "~/.config/emacs/custom.el")
-(load custom-file t)
+;; remember recent opened files
+(recentf-mode 1)
+
+;; remembering minibuffer prompt history
+(setq history-length 25)
+(savehist-mode 1)
+
+;; remember and restore the last place you visited in a file
+(save-place-mode 1)
+
+;; Don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+
+;; Revert buffers when the underlying file has changed
+(global-auto-revert-mode 1)
+(setq global-auto-revert-non-file-buffers t)
+
+;;========================================
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror 'nomessage t)
 
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
 (setq auto-save-file-name-transforms
-      '((".*" "~/.config/emacs/auto-save-list/" t)))
+     '((".*"  "~/.config/emacs/auto-save-list" t)))
 
 (setq backup-directory-alist
-      '((".*" . "~/.config/emacs/backups")))
+     '((".*" . "~/.config/emacs/backups")))
 
 ;;; Presetup End
 ;;========================================
+
+
+
+;;========================================
+;;TODO:
+;;; Plugins
+
+;;=======================================
+;;; Magit Begin
+
+;;; Magit End
+;;=======================================
+
+;; Ivy completion
+
+;; company completion - for code/text completion
+
+;;=======================================
+;;; Lsp Begin
+
+;;; Lsp End
+;;=======================================
+
+;;=======================================
+;;; Magit Begin
+
+;;; Magit End
+;;=======================================
