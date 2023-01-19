724427e4718d53c05a386d330de599d774f705dc
agungTuanany <agung.tuanany@gmail.com>
2023-01-19 14:58:04 +0700

    refactor [emacs]: ignore ".cache" directory from git

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -3,10 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package evil magit)))
+ '(package-selected-packages
+   '(treemacs winum evil-nerd-commenter evil-surround evil-collection rainbow-delimiters doom-themes use-package evil magit))
+ '(warning-suppress-log-types '((use-package) (use-package) (use-package)))
+ '(warning-suppress-types '((use-package) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(line-number-current-line ((t (:inherit default :foreground "#CFC0C5" :slant italic :weight bold))))
  '(whitespace-tab ((t (:foreground "#636363")))))
