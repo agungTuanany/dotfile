71d34bcb74ca613c6e24d13bb99fbb16005b9e4a
agungTuanany <agung.tuanany@gmail.com>
2023-01-26 19:03:34 +0700

    refactor [emacs]: lsp setup + rust
    need more analysis on lsp like:
    
      - performance problem setup
      - setup all .lsp-session-* in one directory

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ob-rust which-key doom-modeline pdf-tools company-box consult orderless vertico marginalia company treemacs winum evil-nerd-commenter evil-surround evil-collection rainbow-delimiters doom-themes use-package evil magit)))
+   '(flycheck rust-mode lsp-mode ob-rust which-key doom-modeline pdf-tools company-box consult orderless vertico marginalia company treemacs winum evil-nerd-commenter evil-surround evil-collection rainbow-delimiters doom-themes use-package evil magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
