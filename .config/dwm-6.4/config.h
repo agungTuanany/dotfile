f7d03202a67dc0f91defe9a8931688e398b26dc9
agungTuanany <agung.tuanany@gmail.com>
2023-01-27 19:32:52 +0700

    refactor [dwm]: add emacs keybinding as default

--- .config/dwm-6.4/config.h
+++ .config/dwm-6.4/config.h
@@ -59,12 +59,16 @@ static const Layout layouts[] = {
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browser[]  = { "firefox-developer-edition", NULL };
+static const char *emacs[]  = { "emacs", NULL };
+static const char *emacs1[]  = { "emacs --debug-init", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = browser } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = emacs } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = emacs1 } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
