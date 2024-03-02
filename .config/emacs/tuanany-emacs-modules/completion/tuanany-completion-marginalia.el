d07bf816a56f5e33d9f1506452966b9d737c7d9a
agungTuanany <agung.tuanany@gmail.com>
2024-03-03 04:25:44 +0700

    refactor [emacs]: refactor completion module
    Ref: https://github.com/gopar/.emacs.d?tab=readme-ov-file#code-completion

---
+++ .config/emacs/tuanany-emacs-modules/completion/tuanany-completion-marginalia.el
@@ -0,0 +1,36 @@
+;;;; tuanany-completion-marginalia-.el --- completion -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2024 Agung Tuanany 
+
+;; Author: Agung Tuanany <agung.tuanany@gmail.com>
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: completion
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+
+;;;; Code:
+(use-package marginalia
+  :ensure
+  :init
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
