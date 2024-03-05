6a40145e79392b27b05c5958d06e719d52b5e3ef
agungTuanany <agung.tuanany@gmail.com>
2024-03-05 17:39:11 +0700

    refactor [emacs]: refactor tools module

---
+++ .config/emacs/tuanany-emacs-modules/tuanany-tools-browse-url.el
@@ -0,0 +1,41 @@
+;;;; tuanany-tools-browse-url.el --- browse url -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2024 Agung Tuanany 
+
+;; Author: Agung Tuanany <agung.tuanany@gmail.com>
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: browser
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+
+;;;; Code:
+
+;; (use-package browse-url
+;;   :ensure nil
+;;   :custom
+;;   ;; Emacs can't find browser binaries
+;;   ;; (browse-url-chrome-program "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
+;;   (browse-url-firefox-program "/usr/bin/firefox-developer-edition")
+;;   ;; Neat trick to open that route to different places
+;;   (browse-url-firefox-new-window-is-tab t)
+;;   :config
+;;   (put 'browse-url-handlers 'safe-local-variable (lambda (x) t)))
---
+++ .config/emacs/tuanany-emacs-modules/tuanany-tools-eww.el
@@ -0,0 +1,38 @@
+;;;; tuanany-tools-eww.el --- eww -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2024 Agung Tuanany 
+
+;; Author: Agung Tuanany <agung.tuanany@gmail.com>
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: browser
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+
+;;;; Code:
+
+;; (use-package eww
+;;   :defer t
+;;   :init
+;;   (add-hook 'eww-after-render-hook #'shrface-mode)
+;;   ;; (add-hook 'eww-mode-hook 'ewnium-mode)
+;;   :config
+;;   (require 'shrface))
