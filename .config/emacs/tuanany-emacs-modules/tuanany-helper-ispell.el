9d270622776c1af86ecdc86943eaaa0a6cf8f533
agungTuanany <agung.tuanany@gmail.com>
2024-03-05 17:28:46 +0700

    refactor [emacs]: refactor helper module
    - Not use LSP any more, cause eat memory as much as you have. Instead
      I use dumb-jump.el.
    - tuanany-helper-helpful.el is so powerful. Nice I have in my belt
    - tuanany-helper-ispell.el use 'aspel (replacemen for Ispell)

---
+++ .config/emacs/tuanany-emacs-modules/tuanany-helper-ispell.el
@@ -0,0 +1,67 @@
+;;;; tuanany-helper-ispell.el --- ispell -*- lexical-binding: t -*-
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: spell checker
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+;; Try to check the reference below, it's a great to start.
+;; Ref: - http://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs/
+;;      - https://github.com/redguardtoo/emacs.d
+;;      - https://github.com/gopar/.emacs.d?tab=readme-ov-file#spelling
+
+;;;; Code:
+
+(use-package ispell
+  :ensure nil
+  :custom
+  (ispell-program-name "aspell")
+  (ispell-personal-dictionary (concat user-emacs-directory "etc/.aspell.lang.pws"))
+  (ispell-dictionary nil)
+  (ispell-local-dictionary nil)
+  (ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"
+                       "--run-together" "--run-together-limit=16"
+                       "--camel-case"))
+  :init
+  (defun tuanany-add-word-to-dictionary ()
+    (interactive)
+    (let ((word (word-at-point)))
+      (append-to-file (concat word "\n") nil ispell-personal-dictionary)
+      (message "Added '%s' to %s" word ispell-personal-dictionary))))
+
+(use-package flyspell
+  :ensure nil
+  :defer
+  :hook ((prog-mode . flyspell-prog-mode)
+         (org-mode . flyspell-mode)
+         (text-mode . flyspell-mode)
+         (flyspell-mode . (lambda ()
+                            (set-face-attribute 'flyspell-incorrect nil :underline '(:style wave :color "Red1"))
+                            (set-face-attribute 'flyspell-duplicate nil :underline '(:style wave :color "DarkOrange")))))
+  :bind (:map flyspell-mode-map
+              ("C-;" . nil)
+              ("C-," . flyspell-goto-next-error)
+              ("C-." . flyspell-auto-correct-word)))
+
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+
+(dolist (hook '(change-log-mode-hook log-edit-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode -1))))
---
+++ .config/emacs/tuanany-emacs-modules/tuanany-helper-minibuffer.el
@@ -0,0 +1,38 @@
+;;;; tuanany-helper-minibuffer.el --- minibuffer -*- lexical-binding: t -*-
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: spell checker
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+;; Try to check the reference below, it's a great to start.
+;; Ref: - http://blog.binchen.org/posts/what-s-the-best-spell-check-set-up-in-emacs/
+;;      - https://github.com/redguardtoo/emacs.d
+;;      - https://github.com/gopar/.emacs.d?tab=readme-ov-file#spelling
+
+;;;; Code:
+
+;; It may also be wise to raise gc-cons-threshold while the minibuffer is active,
+;; so the GC doesn't slow down expensive commands (or completion frameworks, like
+;; helm and ivy. The following is taken from doom-emacs
+(use-package minibuffer
+  :ensure nil
+  :custom
+  (completion-styles '(initials partial-completion flex)))
