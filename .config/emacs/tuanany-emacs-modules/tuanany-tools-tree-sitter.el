f2067d189ff382fe0a5cf917eaa181b7654f0971
agungTuanany <agung.tuanany@gmail.com>
2024-04-10 20:16:15 +0700

    refactor [emacs]: config tree-sitter.el
    Todo:
      - need to research how to use it. So slowly but sure in progress for
      this tree-sitter

---
+++ .config/emacs/tuanany-emacs-modules/tuanany-tools-tree-sitter.el
@@ -0,0 +1,55 @@
+;;;; tuanany-helper-tree-sitter-.el --- tree-sitter -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2024 Agung Tuanany 
+
+;; Author: Agung Tuanany <agung.tuanany@gmail.com>
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: tree-sitter, treesit
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+;; Configuration for tree-sitter
+
+;;;; Code:
+
+(use-package tree-sitter
+  :hook
+  ((
+    css-mode
+    elisp-mode
+    js-mode
+    json-mode
+    php-mode
+    ruby-mode
+    rust-mode
+    sh-mode
+    terraform-mode
+    typescript-mode
+    yaml-mode
+    ) . tuanany-tree-sitter-mode-enable)
+  :preface
+  (defun tuanany-tree-sitter-mode-enable ()
+    (tree-sitter-mode t))
+  :defer t)
+
+(use-package tree-sitter-langs
+  :hook
+  (tree-sitter-after-on . tree-sitter-hl-mode))
