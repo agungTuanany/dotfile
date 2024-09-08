99e020fa3a8d1661eb50c11d3b1944218dedb07b
agungTuanany <agung.tuanany@gmail.com>
2024-09-08 15:47:08 +0700

    feature [emacs]: create a tuanany-tools-grep.el
    Config all 'grep' in different file, not include to init.el

---
+++ .config/emacs/tuanany-emacs-modules/tuanany-tools-grep.el
@@ -0,0 +1,41 @@
+;;;; tuanany-tools-grep.el --- grep -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2024 Agung Tuanany
+
+;; Author: Agung Tuanany <agung.tuanany@gmail.com>
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: process monitor
+
+;;;; Version: 0.1.0
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+
+;;;; Code:
+
+(use-package grep
+  :ensure nil   ; grep is built-in, so no need to run before loaded.
+  :config
+  (setq grep-find-ignored-directories '("tmp" "elpa" ".git" "build" "dist" "transient"))
+  (setq-default grep-highlight-matches t
+                grep-scroll-output t)
+  )
+
+;;; tuanany-tools-grep.el ends here
