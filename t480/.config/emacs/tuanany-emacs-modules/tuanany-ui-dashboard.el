6fd7e0a1725065662e37476100bbebdb89943833
agungTuanany <agung.tuanany@gmail.com>
2025-04-22 08:39:48 +0700

    refactor [emacs]: add a sentence - dashboard-logo-title

--- t480/.config/emacs/tuanany-emacs-modules/tuanany-ui-dashboard.el
+++ t480/.config/emacs/tuanany-emacs-modules/tuanany-ui-dashboard.el
@@ -32,7 +32,7 @@
 (use-package dashboard
   :config
   (setq dashboard-startup-banner 'ascii)
-  (setq dashboard-banner-logo-title "Hello Agung Jangan Malas, ingat Aly dan Istrimu")
+  (setq dashboard-banner-logo-title "Hello Agung Jangan Malas, ingat Nuha, Aly dan Istrimu")
   (setq dashboard-footer-messages '("Take small steps and don't get discouraged. Good luck"
                                     "an idiot admires complexity, a genius admires simplicity"
                                     "Before the endgames, the Gods have placed the middle game, -Siegbert Tarrasch"
