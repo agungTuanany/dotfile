a0a3b1d47a42b8660e8d6892c9070d8ebde3bfa1
agungTuanany <agung.tuanany@gmail.com>
2025-08-30 17:19:04 +0700

    t480 - refactor [emacs]: new file - embark-consult.el

---
+++ t480/.config/emacs/tuanany-emacs-modules/tuanany-completion-embark-consult.el
@@ -0,0 +1,49 @@
+;;;; tuanany-completion-embark-embark.el --- completion -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021-2024 Agung Tuanany
+
+;; Author: Agung Tuanany <agung.tuanany@gmail.com>
+;; URL: http://github.com/agungTuanany/dotfile
+;; Package-Requires: ((emacs "^25.1"))
+;; Created: 2023
+;; Version: 0.1.0
+;; Keywords: completion
+
+;;;; Package-Requires:
+
+;;;; License:
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 or the License, or any later version.
+
+;; This program is distributed in the hope it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITINES FOR
+;; A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;;; Commentary:
+;; https://stackoverflow.com/a/10091330/217812
+;;
+;; In summary,
+;; [-] :custom is used for setting variables and faces associated with
+;; the package, before package loaded; while
+;; [-] :config is used for executing Emacs Lisp code to configure or
+;; initialize the package after it is loaded.
+;; You can use both keywords together in a use-package declaration to
+;; fully customize and configure a package to suit your needs.
+
+;; Why you not make
+
+;;; Code
+(use-package embark-consult
+  :ensure t
+  :after (embark consult)
+  :hook
+  ;; This replaces the old (now-removed) `embark-consult-setup`.
+  (embark-collect-mode . embark-consult-preview-minor-mode))
+
+;;; tuanany-completion-embark-consult.el ends here
